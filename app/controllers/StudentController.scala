7061ce06bbea1ea92e33fa02d1422b9d2cf04e19
Maftunbek <prince777_98@mail.ru>
2019-12-17 10:42:10 +0500

    Bootstrap is added and postgres is configured

--- app/controllers/StudentController.scala
+++ app/controllers/StudentController.scala
@@ -8,6 +8,7 @@ import akka.pattern.ask
 import akka.util.Timeout
 import com.typesafe.scalalogging.LazyLogging
 import javax.inject._
+import org.webjars.play.WebJarsUtil
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 import protocols.StudentProtocol.{AddStudent, Student}
@@ -18,7 +19,8 @@ import scala.concurrent.ExecutionContext
 
 @Singleton
 class StudentController @Inject()(val controllerComponents: ControllerComponents,
-                                  @Named("student-manager") val studentManager: ActorRef
+                                  @Named("student-manager") val studentManager: ActorRef,
+                                  implicit val webJarsUtil: WebJarsUtil
                                  )
                                  (implicit val ec: ExecutionContext)
   extends BaseController with LazyLogging {
@@ -26,16 +28,15 @@ class StudentController @Inject()(val controllerComponents: ControllerComponents
   implicit val defaultTimeout: Timeout = Timeout(60.seconds)
 
   def index: Action[AnyContent] = Action {
-    studentPost()
+    studentPost
     Ok(views.html.student(""))
   }
 
-  def studentPost: Action[JsValue] = Action.async(parse.json) { implicit request => {
+  def studentPost = {
     logger.warn(s"Keldi.........")
     (studentManager ? AddStudent(Student(None, "Maftunbek", "Raxmatov", new Date, 123546))).mapTo[Int].map { pr =>
       Ok(Json.toJson(s"ajji: $pr"))
     }
-  }
   }
 
 }
